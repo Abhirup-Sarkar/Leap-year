4a38b513c4b1ed373a4c0cc831791708804123d1
Abhirup Sarkar <76419771+Abhirup-Sarkar@users.noreply.github.com>
2021-07-02 21:34:41 +0530

    wired text area, click button, output

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 var btnTranslate = document.querySelector("#btn-translate");
+var txtInput = document.querySelector('#txt-input')
+var txtOutput = document.querySelector('#txt-output')
+
+txtOutput.innerText = "Your sentence will be translated here";
+txtInput.innerText = "Enter your sentence here";
 
 
-function clickHandler(){
+function clickHandler(input){
     console.log("clicked!");
+    console.log("input: ",txtInput.value);
+    txtOutput.innerText = txtInput.value;
 }
 btnTranslate.addEventListener("click",clickHandler);
+
