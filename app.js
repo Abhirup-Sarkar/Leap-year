99fdeb6a1ed1a010ee0f8d1968d6cfa3cc09cd04
Abhirup Sarkar <76419771+Abhirup-Sarkar@users.noreply.github.com>
2021-07-02 23:16:26 +0530

    styled the page

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var btnTranslate = document.querySelector("#btn-translate");
 var yearInput = document.querySelector('#txt-input')
 var txtOutput = document.querySelector('#txt-output')
 
-txtOutput.innerText = "Your sentence will be translated here";
+txtOutput.innerText = "Your output will be shown here";
 yearInput.innerText = "Enter your year of birth";
 
 
@@ -12,20 +12,19 @@ function clickHandler(input){
     console.log("clicked!");
     console.log(yearInput.value);
     input = yearInput.value
-    if(input % 400 === 0 ){
-        if(input % 4 == 0 && input % 100 !==0){
-            
-            txtOutput.innerText = input+" is a leap year";
-        }
-        else{
-            txtOutput.innerText = input+" is not a leap year";
-        }
-        
+    if((input % 4 == 0) && (input % 100 !==0) || (input % 400 === 0) ){
+        txtOutput.innerText = input+" is a leap year";
     }
     else{
-        txtOutput.innerText = input+" is not leap year";
-    
+        txtOutput.innerText = input+" is not a leap year";
     }
+          
+            
+        
+        
+   
+        
+   
     
     
     
